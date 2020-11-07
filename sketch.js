f19abbd4264713570ee2c0ba8fb337927905aa86
MarielQ12 <73045394+MarielQ12@users.noreply.github.com>
2020-11-07 10:17:13 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground, box1, box2, box3;
+var packageBody,helicopterBody, ground, box1, box2, box3;
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -15,7 +15,6 @@ function setup() {
 	createCanvas(800, 700);
 	rectMode(CENTER);
 	
-
 	packageSprite=createSprite(width/2, 80, 10,10);
 	packageSprite.addImage(packageIMG)
 	packageSprite.scale=0.2
@@ -42,7 +41,7 @@ function setup() {
 
 	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:.75, isStatic:true});
 	World.add(world, packageBody);
-	
+
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
 	World.add(world, ground);
 	 
@@ -66,18 +65,38 @@ function draw() {
   background(0);
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
+ 
   drawSprites();
  
 }
 
 function keyPressed() {
- if (keyCode === DOWN_ARROW) {
-	Matter.Body.setStatic(packageBody, false);
-
-    // Look at the hints in the document and understand how to make the package body fall only on
-    
-  }
+ 
+if (keyCode === LEFT_ARROW) { 
+	helicopterSprite.x= helicopterSprite.x-20; 
+	//packageSprite.x= packageSprite.x-20; 
+	translation={x:-20,y:0} 
+	
+	//Matter.Body.translate(helicopterSprite, translation) 
+	Matter.Body.translate(packageBody, translation) 
 }
 
+if (keyCode === RIGHT_ARROW) { 
+	helicopterSprite.x= helicopterSprite.x+20; 
+	//packageSprite.x= packageSprite.x+20; 
+	translation={x:20,y:0} 
+	
+	//Matter.Body.translate(helicopterSprite, translation) 
+	Matter.Body.translate(packageBody, translation) 
+}
 
+if (keyCode === DOWN_ARROW) {
+	Matter.Body.setStatic(packageBody, false);
+ }
 
+ if (keyCode === "A") {
+	Matter.Body.setStatic(packageBody, true);
+ }
+
+   }
+  
